ea4407e204a4a4bb294571e69e7fb5463f4c07cc
Guillem-Cuello <guillem.cuello@estudiantat.upc.edu>
2024-10-10 17:53:44 +0200

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,16 @@
 #Codigo para problema 1
 
+#a) Multiple R-squared = coeficient de determinació o de bondat d'ajust, en aquest cas: R^2= 0.1923
+
+#b) Sqrt(Multiple R-squared) = Coeficient de correlació:
+sqrt(0.1923)
+
+#c)Quant augmenta el v. del mercat per un augment de 1 unitat dels actius? -> y = v. mercat = mx + n
+#on m = pendent i x = actius, per tant, per un augment de 1 unitat fem 1*m = m
+m <- 0.06576
+
+#d) Calculem el valor de la recta quan x = 647.1:
+b <- 11.79755
+x <- 647.1
+
+m*x + b
--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,27 @@
 #Codigo para problema 2
+
+mis_dades <- iris
+mis_dades
+
+#Regressió per mínims quadrats -> calculem el que necessitem i apliquem la fòrmula
+
+x<-mis_dades$Petal.Length
+y<-mis_dades$Sepal.Length
+plot(x,y)
+
+x_bar <- mean(x)
+y_bar <- mean(y)
+
+#a) Càlcul del pendent (m):
+
+m<-sum((x-x_bar)*(y-y_bar))/sum((x-x_bar)^2)
+m
+
+#b) #Càlcul del punt de tall amb l'eix vertical (b):
+
+b<-y_bar-m*x_bar #càlcul punt de tall
+b
+
+#c) Predicció quan Petal.Length = 1.5 = x:
+m*1.5 + b
+
